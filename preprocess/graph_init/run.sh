cc214f91508308c99772b922d81e680cceba1184
QMJJ <59856298+Fatfata@users.noreply.github.com>
2024-07-06 19:35:30 +0800

    Update run.sh

--- preprocess/graph_init/run.sh
+++ preprocess/graph_init/run.sh
@@ -3,3 +3,6 @@ python init_graph.py --dataset 'Weibo' --max_nodes 100
 
 # Twitter
 python init_graph.py --dataset 'Twitter' --max_nodes 100
+
+# GossipCop
+python init_graph.py --dataset 'gossip' --max_nodes 100
