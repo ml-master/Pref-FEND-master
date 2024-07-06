1d8407de24a4d95b7d3fab1a1fb70b894dce37c2
QMJJ <59856298+Fatfata@users.noreply.github.com>
2024-07-06 19:34:28 +0800

    Update run.sh

--- preprocess/EANN_Text/run.sh
+++ preprocess/EANN_Text/run.sh
@@ -3,3 +3,6 @@ python events_clustering.py --dataset 'Weibo' --events_num 300
 
 # Twitter
 python events_clustering.py --dataset 'Twitter' --events_num 300
+
+# GossipCop
+python events_clustering.py --dataset 'gossip' --events_num 300
